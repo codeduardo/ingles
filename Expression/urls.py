f4168c3135ffade1e27fbdc79eabf2142eb4193a
EduJaucha <agredu1941@gmail.com>
2021-07-03 12:41:47 -0500

    second_deploy

--- Expression/urls.py
+++ Expression/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.show_expr,name="Expression"),
-    path('/delete_expr/<int:id>',views.del_expr,name="del_expr"),
+    path('delete_expr/<int:id>/',views.del_expr,name="del_expr"),
 ]
 
