09d12d1a5dd66625f72ea3058e482a3f1e6fc728
EduJaucha <agredu1941@gmail.com>
2021-07-03 12:54:39 -0500

    third_deploy

--- Ingles/settings.py
+++ Ingles/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!nt!sali-y9$)zxdhmj^%-$!dlte)a8js%s4^3l2)*wmcr-ke('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['python-english.herokuapp.com/']
 
 
 # Application definition
